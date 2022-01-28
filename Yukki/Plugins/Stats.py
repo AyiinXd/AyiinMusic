ddc4397888845e45284972a59e3ade9a9ad9bd37
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-28 12:33:56 +0700

    Update Stats.py

--- Yukki/Plugins/Stats.py
+++ Yukki/Plugins/Stats.py
@@ -60,7 +60,7 @@ async def gstats(_, message):
         pass
     uptime = await bot_sys_stats()
     response = await message.reply_photo(
-        photo="Utils/Query.jpg", caption="Getting Stats!"
+        photo="Utils/IMG_20220127_230942_584.jpg", caption="Getting Stats!"
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000
