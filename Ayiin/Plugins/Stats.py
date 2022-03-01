e28f66dcf0d9ff4cf864655c2240a576764b2a77
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-03-02 00:45:25 +0800

    Update Stats.py

--- Ayiin/Plugins/Stats.py
+++ Ayiin/Plugins/Stats.py
@@ -60,7 +60,7 @@ async def gstats(_, message):
         pass
     uptime = await bot_sys_stats()
     response = await message.reply_photo(
-        photo="Utils/icon.gif", caption="Getting Stats!"
+        photo="Utils/ayiin.jpg", caption="Getting Stats!"
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000
